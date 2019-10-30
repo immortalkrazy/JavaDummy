8069285b26d72b716cd2c6f763e1924f76e3dd49
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-10-29 22:37:26 -0400

    updated helloapp2

--- JavaDummy/src/HelloApp2.java
+++ JavaDummy/src/HelloApp2.java
@@ -3,8 +3,9 @@ public class HelloApp2 {
 
       public static void main(String[] args) {
 
-            // Greetings myGreetings = new Greetings();
-            // myGreetings.sayHello();
+            Greetings myGreetings = new Greetings();
+            myGreetings.sayHello();
+
             Greeter myGreeter = new Greeter();
             myGreeter.sayHello();
 
