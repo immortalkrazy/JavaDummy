f64cf21b6fa1facfa68401490ec92b9a5faf2b26
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-12-20 21:32:14 -0500

    cloneTest created

---
+++ JavaDummy/src/CloneTest.java
@@ -0,0 +1,73 @@
+
+public class CloneTest {
+
+      public static void main(String[] args) {
+
+            Employee emp1 = new Employee("James", "Bond");
+            emp1.setSalary(40000.00);
+
+            Employee emp2 = (Employee) emp1.clone();
+
+            emp1.setLastName("Smith");
+
+            System.out.println(emp1);
+            System.out.println(emp2);
+
+      }
+
+      private static class Employee {
+
+            private String firstName;
+            private String lastName;
+            private double salary;
+
+            public Employee(String firstName, String lastName) {
+                  super();
+                  this.firstName = firstName;
+                  this.lastName = lastName;
+            }
+
+            @SuppressWarnings("unused")
+            public String getFirstName() {
+                  return firstName;
+            }
+
+            @SuppressWarnings("unused")
+            public void setFirstName(String firstName) {
+                  this.firstName = firstName;
+            }
+
+            @SuppressWarnings("unused")
+            public String getLastName() {
+                  return lastName;
+            }
+
+            public void setLastName(String lastName) {
+                  this.lastName = lastName;
+            }
+
+            @SuppressWarnings("unused")
+            public double getSalary() {
+                  return salary;
+            }
+
+            public void setSalary(double salary) {
+                  this.salary = salary;
+            }
+
+            @Override
+            public String toString() {
+                  return this.getClass().getName() + " [" + this.firstName + " " + this.lastName + ", " + this.salary
+                              + "]";
+            }
+
+            public Object clone() {
+                  Employee employee;
+                  employee = new Employee(this.firstName, this.lastName);
+                  employee.setSalary(this.salary);
+                  return employee;
+            }
+
+      }
+
+}
